79e9ce5e69199db1a34dba30e3dbd9d24770d747
Sonata0xff <2080782895@qq.com>
2025-10-07 14:15:58 +0800

    add TIM1 counter template code and doc

--- user/inc/head.h
+++ user/inc/head.h
@@ -1,5 +1,5 @@
 #ifndef HEAD_H
 #define HEAD_H
 #include "stm32f10x.h"                  // Device header
-void EXTI9_5_IRQHandler(); //interrupt func
+void TIM1_UP_IRQHandler();
 #endif
--- user/src/core.c
+++ user/src/core.c
@@ -1,10 +1,7 @@
 #include "head.h"
-
-void EXTI9_5_IRQHandler()
+void TIM1_UP_IRQHandler()
 {
-	if (EXTI_GetITStatus(EXTI_Line5) == SET) {
-		EXTI_ClearITPendingBit(EXTI_Line5);
-		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)) GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-		else GPIO_SetBits(GPIOA, GPIO_Pin_0);
-	} else return; 
+	if (GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_0)) GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+	else GPIO_SetBits(GPIOA, GPIO_Pin_0);
+	TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
 }
