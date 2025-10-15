a970c161e212be9bdd5ae830fe71aeb63b1f003f
Sonata0xff <2080782895@qq.com>
2025-10-16 00:14:12 +0800

    add encoding interface template code

--- user/src/main.c
+++ user/src/main.c
@@ -5,44 +5,44 @@
 #include "head.h"
 int main()
 {
-	/*get pwm value*/
-	uint16_t timer_period_us = 1000;
-	float emp_precent = 0.5;
-	uint16_t wholePeriod = (uint16_t)(timer_period_us / 2);
-	uint16_t lowPeriod = (uint16_t)(wholePeriod * emp_precent);
-	/*init outport gpio*/
+	/*init GPIO*/
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	GPIO_InitTypeDef outport_config = {
+	GPIO_InitTypeDef ti1_port = {
 		.GPIO_Pin = GPIO_Pin_8,
 		.GPIO_Speed = GPIO_Speed_50MHz,
-		.GPIO_Mode = GPIO_Mode_AF_PP
+		.GPIO_Mode = GPIO_Mode_IN_FLOATING
 	};
-	GPIO_Init(GPIOA, &outport_config);
-	/*init timer1*/
+	GPIO_InitTypeDef ti2_port = {
+		.GPIO_Pin = GPIO_Pin_9,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_Mode = GPIO_Mode_IN_FLOATING
+	};
+	GPIO_InitTypeDef ex_port = {
+		.GPIO_Pin = GPIO_Pin_0,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_Mode = GPIO_Mode_Out_PP
+	};
+	GPIO_Init(GPIOA, &ti1_port);
+	GPIO_Init(GPIOA, &ti2_port);
+	GPIO_Init(GPIOA, &ex_port);
+	/*timer init*/
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
-	TIM_InternalClockConfig(TIM1);
+	TIM_EncoderInterfaceConfig(TIM1, TIM_EncoderMode_TI12, TIM_ICPolarity_BothEdge, TIM_ICPolarity_BothEdge);
 	TIM_TimeBaseInitTypeDef base_config = {
-		.TIM_Prescaler = 72 - 1,
-		.TIM_CounterMode = TIM_CounterMode_CenterAligned1,
-		.TIM_Period = wholePeriod,
+		.TIM_Prescaler = 1 - 1,
+		.TIM_CounterMode = TIM_CounterMode_Up,
+		.TIM_Period = 1000 - 1,
 		.TIM_ClockDivision = TIM_CKD_DIV2,
 		.TIM_RepetitionCounter = 0
 	};
 	TIM_TimeBaseInit(TIM1, &base_config);
-	TIM_OCInitTypeDef oc_config = {
-		.TIM_OCMode = TIM_OCMode_Toggle,
-		.TIM_OutputState = TIM_OutputState_Enable,
-		.TIM_OutputNState = TIM_OutputNState_Disable,
-		.TIM_Pulse = lowPeriod,
-		.TIM_OCPolarity = TIM_OCPolarity_High,
-		.TIM_OCNPolarity = TIM_OCNPolarity_High,
-		.TIM_OCIdleState = TIM_OCIdleState_Reset,
-		.TIM_OCNIdleState = TIM_OCNIdleState_Reset
-	};
-	TIM_CtrlPWMOutputs(TIM1, ENABLE);
-	TIM_OC1Init(TIM1, &oc_config);
-	/*start system*/
+	/*start counter*/
 	TIM_Cmd(TIM1, ENABLE);
-	while(1);
+	while(1) {
+		if (TIM_GetFlagStatus(TIM1, TIM_FLAG_Update) == RESET) continue;
+		TIM_ClearFlag(TIM1, TIM_FLAG_Update);
+		if (GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_0) != 0) GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+		else GPIO_SetBits(GPIOA, GPIO_Pin_0);
+	}
 	return 0;
 }
